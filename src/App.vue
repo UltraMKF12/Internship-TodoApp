e400f21fc58bf34a21518a123304ef6888421c89
Jozsef Szeri <harcos212@gmail.com>
2024-03-12 14:41:54 +0200

    ITL-7 Create the header component
    Also deleted default some auto-generated vue components and a files that came with the project setup.

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import TodoHeader from './components/TodoHeader.vue'
+</script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <TodoHeader />
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
