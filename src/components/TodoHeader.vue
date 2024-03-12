297ee8364a53ccf1ee727d7222bd2995122f998f
Jozsef Szeri <harcos212@gmail.com>
2024-03-12 15:40:33 +0200

    ITL-10 Create the event that triggers the todo creation functionality
    The button, when pressed, emits addTodo

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import PlusSymbol from './icons/plus_icon.vue'
+defineEmits(['addTodo'])
 </script>
 
 <template>
   <div class="flex justify-between">
     <h1 class="text-7xl font-bold">To do List</h1>
-    <button>
+    <button @click="$emit('addTodo')">
       <PlusSymbol />
     </button>
   </div>
