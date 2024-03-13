99c9c6b4869ba394fe19c1aff9c4d1629ec7ef2b
Jozsef Szeri <harcos212@gmail.com>
2024-03-13 09:46:38 +0200

    Added requested changes
    Deleted the icon.vue, the PlusButtonIcon is now outside of BaseButton, modified base button according to the request, but don't know if it's correct.

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,5 +1,16 @@
 <template>
-  <slot></slot>
+  <button></button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+interface Props {
+  label: string
+  disabled: boolean
+  textColor: string
+  backgroundColor: string
+  buttonText: string
+}
+
+defineProps<Props>()
+defineEmits(['click'])
+</script>
--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,14 +1,11 @@
 <template>
   <div class="flex justify-between">
     <h1 class="text-7xl font-bold">To do List</h1>
-    <BaseButton>
-      <PlusCircleIcon @click="$emit('addTodo')" class="h-16 w-16 text-green-400 cursor-pointer" />
-    </BaseButton>
+    <PlusCircleIcon @click="$emit('addTodo')" class="h-16 w-16 text-green-400 cursor-pointer" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { PlusCircleIcon } from '@heroicons/vue/24/solid'
-import BaseButton from '@/components/BaseButton.vue'
 defineEmits(['addTodo'])
 </script>
