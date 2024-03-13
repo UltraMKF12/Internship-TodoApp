55494e26f0ad371162c38427742134f35ab2dc24
Jozsef Szeri <harcos212@gmail.com>
2024-03-13 12:32:43 +0200

    ITL 11 Create the Placeholder img when the user is in the home page and no todos added to todo list
    Also made Todo Interface

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="m-auto my-6 flex max-w-screen-md flex-col content-center justify-center">
     <TodoHeader />
+    <TodoListEmpty v-if="isTodoListEmpty" />
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed, ref } from 'vue'
 import TodoHeader from '@/components/TodoHeader.vue'
+import TodoListEmpty from './components/TodoListEmpty.vue'
+
+interface Todo {
+  id: number
+  priority: 'high' | 'medium' | 'low'
+  title: string
+  description: string
+}
+
+const todoList = ref<Todo[]>([])
+const isTodoListEmpty = computed(() => !todoList.value.length)
 </script>
---
+++ src/components/TodoListEmpty.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex select-none flex-col items-center justify-center">
+    <NoTodoImage />
+    <p class="text-4xl font-bold text-gray-400">You have no todos yet</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import NoTodoImage from '@/components/icons/NoTodoImage.vue'
+</script>
